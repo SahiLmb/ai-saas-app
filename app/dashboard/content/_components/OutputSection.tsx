cb6fe306653405431cd8b258456d8154faaa41a1
Sahil <sahilbodke10@gmail.com>
2024-09-05 01:14:45 +0530

    form and output sect inside slug-id

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,17 +1,30 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 import { Editor } from '@toast-ui/react-editor';
 import { Copy } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
-function OutputSection() {
+interface props{
+  aiOutput:string;
+}
+
+function OutputSection({aiOutput}:props) {
   const editorRef:any=useRef();
+
+useEffect(()=>{
+  const editorInstance=editorRef.current.getInstance();
+  editorInstance.setMarkdown(aiOutput);
+},[aiOutput])
+
+
   return (
     <div className='bg-white shadow-lg border rounded-lg'>
       <div className='flex justify-between items-center p-5'>
       <h2 className="font-medium text-lg">Your Result</h2>
-        <Button className="flex gap-2">
+        <Button className="flex gap-2"
+        onClick={()=>navigator.clipboard.writeText(aiOutput)}
+        >
           <Copy className="w-4 h-4"/>
           Copy
         </Button>
