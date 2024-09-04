d38cbeded470d157a3c22b4e287e53b03edcc09d
Sahil <sahilbodke10@gmail.com>
2024-09-05 01:14:12 +0530

    implement ai

--- app/dashboard/content/[slug-id]/page.tsx
+++ app/dashboard/content/[slug-id]/page.tsx
@@ -1,27 +1,87 @@
 "use client"
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 import OutputSection from '../_components/OutputSection'
-import Templates from '@/app/(info)/Templates'
+import Templates from '@/app/(info)/Template'
 import { TEMPLATE } from '../../_components/TemplateListSection'
 import FormSection from '../_components/FormSection'
 import { ArrowLeft } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { chatSession } from '@/utils/AiModal'
+import Template from '@/app/(info)/Template'
+import { AIOutput } from '@/utils/schema'
+import { db } from '@/utils/db'
+import { useUser } from '@clerk/nextjs'
+import moment from 'moment';
+import { TotalUsageContext } from '@/app/(context)/TotalUsageContext'
+import { useRouter } from 'next/navigation'
+import { UserSubscriptionContext } from '@/app/(context)/UserSubscriptionContext'
+import { UpdateCreditUsageContext } from '@/app/(context)/UpdateCreditUsageContext'
+
 
 interface PROPS{
     params:{
-        'template-slug':string
+        'slug-id':string
     }
 }
 function CreateNewContent(props:PROPS) {
 
-        const selectedTemplate:TEMPLATE|undefined=Templates?.find((item)=>item.slug==props.params['template-slug']);
+        const selectedTemplate: TEMPLATE| undefined=Template?.find((item)=>item.slug==props.params['slug-id']);
+        const [loading,setLoading]=useState(false);
+        const [aiOutput,setAiOutput]=useState<string>('');
+        const {user}=useUser();
+        const router=useRouter();
+        const {totalUsage,setTotalUsage}=useContext(TotalUsageContext)
+        const {userSubscription,setUserSubscription}=useContext(UserSubscriptionContext);
+        const {UpdateCreditUsage,setUpdateCreditUsage}=useContext(UpdateCreditUsageContext)
+        
+ /**
+   * Used to generate content from AI
+   * @param formData
+   * @returns
+   */
 
-        const GenerateAIContent=(formData:any)=>{
+        const GenerateAIContent=async(formData:any)=>{
+          if(totalUsage>=10000&&!userSubscription)
+          {
+            console.log("Please Upgrade")
+            // once user has exhausted credits redirect to billing page
+            router.push('/dashboard/billing')
+            return;
+          }
+          setLoading(true);
+          const SelectedPrompt=selectedTemplate?.aiPrompt;
+
+          const FinalAIPrompt=JSON.stringify(formData)+", "+SelectedPrompt;
+
+          const result=await chatSession.sendMessage(FinalAIPrompt);
+
+          // console.log(result.response.text());
+          setAiOutput(result?.response.text());
+          await SaveInDb(formData,selectedTemplate?.slug,result?.response.text())
+          setLoading(false);
+
+          setUpdateCreditUsage(Date.now())
+
 
         }
-    // console.log(props.params['template-slug'])
+// Saving data in db
+
+        const SaveInDb=async(formData:any,slug:any,aiResp:string)=>{
+          try {
+          const result =await db.insert(AIOutput).values({
+            formData:formData,
+            templateSlug:slug,
+            aiResponse:aiResp,
+            createdBy:user?.primaryEmailAddress?.emailAddress,
+            createdAt:moment().format("DD/MM/yyyy"),
+          })
+        console.log('Data saved successfully:', result);
+      } catch (error) {
+        console.error('Error saving data:', error);
+      }};
+    // console.log(props.params['slug-id'])
   return (
     // back btn
       <div className='p-10'>
@@ -32,10 +92,12 @@ function CreateNewContent(props:PROPS) {
           {/* FormSection */}
           <FormSection
           selectedTemplate={selectedTemplate}
-          userFormInput={(v:any)=>GenerateAIContent(v)} />
+          userFormInput={(v:any)=>GenerateAIContent(v)} 
+          loading={loading}
+          />
           {/* OutputSection */}
           <div className='col-span-2'>
-          <OutputSection/>
+          <OutputSection aiOutput={aiOutput} />
           </div>
           </div>
      
