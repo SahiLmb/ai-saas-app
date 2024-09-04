f127f5185473e4dca02580457c3dabc59e254ff7
Sahil Bodke <97834129+SahiLmb@users.noreply.github.com>
2024-09-05 01:39:50 +0530

    Delete drizzle.config.js

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./utils/schema.tsx",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://AI-content-gen_owner:bEJlj7gpmN6U@ep-polished-water-a5lmpv87.us-east-2.aws.neon.tech/AI-content-gen?sslmode=require',
-    }
-  };
