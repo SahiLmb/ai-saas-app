b349f842dac2aadc3fb0442bc913807bb1e74090
Sahil <sahilbodke10@gmail.com>
2024-09-05 01:12:37 +0530

    context logic

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+'use client'
+
+import React, { useContext, useState } from 'react'
 import Sidebar from './_components/Sidebar';
 import Header from './_components/Header';
+import { TotalUsageContext } from '../(context)/TotalUsageContext';
+import { UserSubscriptionContext } from "../(context)/UserSubscriptionContext";
+import { CreditUpdateUsageContext, UpdateCreditUsageContext } from '../(context)/UpdateCreditUsageContext';
 
 function layout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+    // credits usage context
+    const [totalUsage,setTotalUsage]=useState<Number>(0); 
+    const [userSubscription,setUserSubscription]=useState<boolean>(false);
+    const [UpdateCreditUsage, setUpdateCreditUsage] = useState<any>()
+
   return (
+    <UserSubscriptionContext.Provider value={{userSubscription,setUserSubscription}}>
+    <TotalUsageContext.Provider value={{totalUsage,setTotalUsage}}>
+      <UpdateCreditUsageContext.Provider value={{UpdateCreditUsage,setUpdateCreditUsage}}>
     <div className='bg-slate-100 h-screen'>
         {/* to hide sidebar when the screen is not medium or large size */}
         <div className='md:w-64 hidden md:block fixed'>
@@ -18,6 +31,9 @@ function layout({
         {children}
         </div>
     </div>
+    </UpdateCreditUsageContext.Provider>
+    </TotalUsageContext.Provider>
+    </UserSubscriptionContext.Provider>
   )
 }
 
