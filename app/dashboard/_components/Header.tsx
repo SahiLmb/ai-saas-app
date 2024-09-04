f0c6752189030b2a2c8e5e325b83fb167051ad5d
Sahil <sahilbodke10@gmail.com>
2024-09-05 01:13:02 +0530

    header and sidenav

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,20 +1,27 @@
 import { Search } from 'lucide-react'
 import React from 'react'
+import UsageTrack from './UsageTrack'
+import { UserButton } from '@clerk/nextjs'
 
 function Header() {
   return (
     <div className='p-5 shadow-sm border-b-2 flex justify-between items-center bg-white'>
-        <div className='flex gap-2 itmes-center p-2 border 
+        {/* <div className='flex gap-2 items-center p-2 border 
         rounded-md max-w-lg bg-white'>
            <Search/>
             <input type='text' placeholder='Search...' 
-            className='outline-none'
+            className='outline-none flex-1'
             />
-        </div>
-        <div>
-            <h2 className='bg-primary p-1 rounded-full text-xs hidden md:block text-white px-2 cursor-pointer '>
+        </div> */}
+        {/* Usage tracker */}
+      <div className='flex-1 mx-5'>
+        <UsageTrack />
+      </div>
+        <div className='flex gap-5 px-3 items-center'>
+            <h2 className='bg-primary p-1 rounded-full text-xs hidden md:block text-white px-2 cursor-pointer whitespace-nowrap'>
               Join Membership just for 799Rs/Month</h2>
         </div>
+        <UserButton/>
     </div>
   )
 }
--- app/dashboard/_components/Sidebar.tsx
+++ app/dashboard/_components/Sidebar.tsx
@@ -4,6 +4,8 @@ import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { Cog, File, FileClock, FolderCog, Home, Wallet, Wallet2, WalletCards } from 'lucide-react'
 import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import UsageTrack from './UsageTrack'
 function Sidebar() {
 
     const MenuList=[
@@ -23,7 +25,7 @@ function Sidebar() {
         },{
             name:'Setting',
             icon:FolderCog,
-            path:'/dashboard/setting'
+            path:'/dashboard/settings'
         },
     ]
 
@@ -33,25 +35,33 @@ function Sidebar() {
     },[])
   // Adding logo
     return (
-    <div className='h-screen p-5 shadow-sm border bg-white '>
+    <div className='h-screen relative p-5 shadow-sm border bg-white '>
         <div className='flex justify-center'>
+        <Link href={'/'}>
         <Image src={'/logo.svg'}
         alt="logo"
         width={45}
         height={45}
         />
+        </Link>
+       
     </div>
     {/* sidebar config */}
         <hr className='my-5' />
     <div className='mt-7 border'>
         {MenuList.map((menu, index)=>(
+            <Link href={menu.path}>
             <div  className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg transition-all cursor-pointer items-center ${
                 path == menu.path && "bg-primary text-white"
               }`}>
                 <menu.icon className='h-6 w-7'/>
                 <h2 className='text-lg'>{menu.name}</h2>
             </div>
+            </Link>
         ))}
+    </div>
+    <div className='absolute bottom-10 left-0 width-full'>
+    {/* <UsageTrack/> */}
     </div>
     </div>
   )
