eb7a787e04f4dad1536330f69bce2c1689256a26
Sahil <sahilbodke10@gmail.com>
2024-09-05 10:27:22 +0530

    commit

--- app/dashboard/_components/UsageTrack.tsx
+++ app/dashboard/_components/UsageTrack.tsx
@@ -1,86 +1,80 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import { db } from '@/utils/db';
-import { AIOutput, UserSubscription } from '@/utils/schema';
-import { useUser } from '@clerk/nextjs';
-import { eq } from 'drizzle-orm';
-import React, { useContext, useEffect, useState } from 'react'
-import { HISTORY } from '../history/page';
-import { TotalUsageContext } from '@/app/(context)/TotalUsageContext';
-import { UserSubscriptionContext } from '@/app/(context)/UserSubscriptionContext';
-import { UpdateCreditUsageContext } from '@/app/(context)/UpdateCreditUsageContext';
-import Link from 'next/link';
+"use client";
+import { Button } from "@/components/ui/button";
+import { db } from "@/utils/db";
+import { AIOutput, UserSubscription } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import React, { useContext, useEffect, useState } from "react";
+import { HISTORY } from "../history/page";
+import { TotalUsageContext } from "@/app/(context)/TotalUsageContext";
+import Link from "next/link";
+import { UserSubscriptionContext } from "@/app/(context)/UserSubscriptionContext";
+import { UpdateCreditUsageContext } from "@/app/(context)/UpdateCreditUsageContext";
 
 function UsageTrack() {
+  const { user } = useUser();
+  const {totalUsage, setTotalUsage} = useContext(TotalUsageContext)
+  const {userSubscription,setUserSubscription} = useContext(UserSubscriptionContext);
+  const [maxWords,setMaxWords]=useState(10000)
+  const {updateCreditUsage,setUpdateCreditUsage}=useContext(UpdateCreditUsageContext)
 
-    const {user}=useUser();
-    const {totalUsage,setTotalUsage}=useContext(TotalUsageContext)
-    const {userSubscription, setUserSubscription}=useContext(UserSubscriptionContext)
-    const [maxWords,setMaxWords]=useState(10000)
-    const {UpdateCreditUsage,setUpdateCreditUsage}=useContext(UpdateCreditUsageContext);
-    
-    
-    useEffect(()=>{
-        user&&GetData();
-        user&&IsUserSubscribed();
-    },[user])
-
-    useEffect(()=>{
-        user&&GetData();
-    },[UpdateCreditUsage&&user])
-
-    const GetData=async()=>{
-    {/*@ts-ignore */}
-        const result:HISTORY[]=await db.select().from(AIOutput)
-        .where(eq(AIOutput.createdBy,user?.primaryEmailAddress?.emailAddress as string));
-
-        GetTotalUsage(result)
-    }
-
-    const IsUserSubscribed= async ()=>{
-        const result=await db.select().from(UserSubscription)
-        .where(eq(UserSubscription.email,user?.primaryEmailAddress?.emailAddress as string));
-        if(result){
-            setUserSubscription(true)
-            // subscribed users get 1000000 credits
-            setMaxWords(1000000);
-          }
-           console.log(result)
-    }
-
-
-// using history to determine the no. of credits used
-
-    const GetTotalUsage=(result:HISTORY[])=>{
-        let total:number=0;
-        result.forEach(element => {
-            total=total+Number(element.aiResponse?.length)
-        })
-
-        setTotalUsage(total)
-        console.log(total);
-    }
   
-  
-    return (
-    <div className='m-5'>
-        <div className='bg-primary text-white p-3 rounded-lg flex flex-col md:flex-row items-center justify-between'>
-            <h2 className='font-medium mb-1'>Credits</h2>
-            <div className='h-2 bg-white w-full rounded-full mt-3 relative'>
-            <div className='h-2 bg-[#d0eb06] rounded-full'
+  useEffect(() => {
+   user&&GetData();
+   user&&IsUserSubscribe();
+  }, [user]);
+
+  useEffect(()=>{
+    user&&GetData();
+  },[updateCreditUsage&&user]);
+
+  const GetData = async()=>{
+    {/*@ts-ignore*/ }
+    const result:HISTORY[] = await db.select().from(AIOutput).where(eq(AIOutput.createdBy, user?.primaryEmailAddress?.emailAddress));
+    GetTotalUsage(result)
+
+  }
+
+  const IsUserSubscribe=async()=>{
+     {/* @ts-ignore */}
+     const result=await db.select().from(UserSubscription).where(eq(UserSubscription.email,user?.primaryEmailAddress?.emailAddress));
+     if(result){
+      setUserSubscription(true)
+      setMaxWords(1000000);
+    }
+     console.log(result)
+  }
+
+  const GetTotalUsage = (result:HISTORY[]) => {
+    let total: number = 0;
+    result.forEach((element) => {
+      total = total + Number(element.aiResponse?.length);
+    });
+    setTotalUsage(total);
+    console.log(total);
+  };
+
+  return (
+    <div className="m-5">
+      <div className="bg-primary text-white p-3 rounded-lg">
+        <h2 className="font-medium mb-2">Credits </h2>
+        <div className="h-2 bg-[#9981f9] w-full rounded-lg mb-2">
+          <div
+            className="h-2 bg-white rounded-full transition-all duration-1000"
             style={{
-                width:(totalUsage/maxWords)*100+"%" // to show percentaged used
+              width:(totalUsage/maxWords)*100+"%",
             }}
-            ></div>
-            </div>
-            <h2 className='text-sm my-2'>{totalUsage}/{maxWords} credits utitlized </h2>
+          ></div>
         </div>
-        <Link href={'/dashboard/billing'}>
-        <Button variant={'secondary'} className='w-full md:w-auto my-3 text-primary'>Upgrade
-        </Button>
-        </Link>
+        <h2 className="text-sm">{totalUsage}/{maxWords} Credit use</h2>
+      </div>
+      <Link href={'/dashboard/billing'}>
+      <Button variant={"secondary"} className="w-full my-2 text-primary">
+        Upgrade
+      </Button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default UsageTrack
+export default UsageTrack;
